0d70c9c97a51c8e9a4951b036f1df191e2fc9d0d
Luan <luanss@id.uff.br>
2022-06-27 17:01:08 -0300

    adicionado modal de modulos

--- resources/js/Components/ModalModule.vue
+++ resources/js/Components/ModalModule.vue
@@ -1,8 +1,9 @@
 <script setup>
 import Modal from "../Jetstream/Modal";
-import { ref } from "vue";
-const moduleManage = ref(true);
+import { ref, reactive } from "vue";
+const moduleManage = ref(false);
 const emit = defineEmits(["close"]);
+const processing = ref(false)
 
 defineProps({
     show: {
@@ -22,6 +23,38 @@ defineProps({
 const close = () => {
     moduleManage.value = false;
 };
+
+function HandleImageModuleUpload(){
+    document.getElementById("module_image_upload").click()
+}
+
+const handleSubmit = () => {
+  processing.value = true;
+  axios
+    .post("api/leads/adicionar", {
+      name: form.nome,
+      appLink: form.link_gerenciamento,
+      manageLink: form.link_gerenciamento,
+      description: form.descricao
+    })
+    .then(() => {
+      processing.value = false;
+    //   leadText.value = ""
+    })
+    .catch((error) => {
+      processing.value = false;
+      console.log("erro");
+    });
+//   store.commit("toggleLeadShow");
+};
+
+const form = reactive({
+   descricao:"",
+   link_gerenciamento:"",
+   link_aplicacao:"",
+   nome:"",
+})
+
 </script>
 
 <template>
@@ -44,7 +77,76 @@ const close = () => {
                     </h3>
 
                     <div class="mt-2">
-                        <slot name="content" />
+                        <div class="form-control w-full">
+                            <label class="label">
+                                <span class="label-text">Nome do módulo?</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Informe o nome do módulo"
+                                class="input input-bordered w-full"
+                                v-model="form.nome"
+                            />
+                            <label class="label">
+                                <span class="label-text"
+                                    >Link da Aplicação</span
+                                >
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Informe o link da aplicação"
+                                class="input input-bordered w-full"
+                                v-model="form.link_aplicaçao"
+                            />
+                            <label class="label">
+                                <span class="label-text"
+                                    >Link da Gerenciamento</span
+                                >
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Informe Link de Gerenciamento"
+                                class="input input-bordered w-full"
+                                v-model="form.link_gerenciamento"
+                            />
+
+                            <label class="label">
+                                <span class="label-text"
+                                    >Descrição do Módulo?</span
+                                >
+                            </label>
+                            <textarea
+                                class="textarea input-bordered w-full"
+                                placeholder="Informe a descrição do módulo"
+                                v-model="form.descricao"
+                            ></textarea>
+                            <input
+                                id="module_image_upload"
+                                type="file"
+                                hidden
+
+                            />
+                            <button
+                                class="btn btn-square btn-outline btn-primary w-full mt-3"
+                                @click="HandleImageModuleUpload"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                                    />
+                                </svg>
+                                Carregar Imagem
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -55,7 +157,7 @@ const close = () => {
                 Cancelar
             </button>
 
-            <button class="btn m-1" @click="HandleCreateModule">
+            <button class="btn m-1" @click="handleSubmit">
                 Confirmar
             </button>
             <!-- <slot name="footer" /> -->
--- resources/js/Components/Modules.vue
+++ resources/js/Components/Modules.vue
@@ -3,58 +3,7 @@
         <template #title> Adicionar módulo </template>
 
         <template #content>
-            <div class="form-control w-full">
-                <label class="label">
-                    <span class="label-text">Nome do módulo?</span>
-                </label>
-                <input
-                    type="text"
-                    placeholder="Informe o nome do módulo"
-                    class="input input-bordered w-full"
-                />
-                <label class="label">
-                    <span class="label-text">Link de Gerenciamento</span>
-                </label>
-                <input
-                    type="text"
-                    placeholder="Informe o link da aplicação"
-                    class="input input-bordered w-full"
-                />
-                <label class="label">
-                    <span class="label-text">Link da Aplicação</span>
-                </label>
-                <input
-                    type="text"
-                    placeholder="Informe Link de Gerenciamento"
-                    class="input input-bordered w-full"
-                />
 
-                <label class="label">
-                    <span class="label-text">Descrição do Módulo?</span>
-                </label>
-                <textarea
-                    class="textarea input-bordered w-full"
-                    placeholder="Informe a descrição do módulo"
-                ></textarea>
-                <input id="module_image_upload" type="file" hidden />
-                <button class="btn btn-square btn-outline btn-primary w-full mt-3" @click="HandleImageModuleUpload">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="2"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                        />
-                    </svg>
-                    Carregar Imagem
-                </button>
-            </div>
         </template>
     </ModalModule>
     <div
@@ -125,25 +74,5 @@
 import { ref } from "vue";
 import { cards } from "../helpers/modulos.js";
 import ModalModule from "../Components/ModalModule";
-function HandleImageModuleUpload(){
-    document.getElementById("module_image_upload").click()
-}
 
-const handleSubmit = () => {
-//   processing.value = true;
-//   console.log(processing.value);
-  axios
-    .post("api/leads/adicionar", {
-      name: leadText.value,
-      lane_id: 1,
-    })
-    .then(() => {
-      processing.value = false;
-      leadText.value = ""
-    })
-    .catch((error) => {
-      console.log("erro");
-    });
-  store.commit("toggleLeadShow");
-};
 </script>
