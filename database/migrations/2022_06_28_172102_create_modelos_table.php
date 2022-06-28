bb8203f6c2937f8ffdf1a61b365dc2e04bd0a480
juniorjrml <jancarlolcj@gmail.com>
2022-06-28 14:25:42 -0300

    alterando equipamento e adicionando modelo(responsavel pela classe do equipamento)

--- database/migrations/2022_06_28_172102_create_modelos_table.php
+++ database/migrations/2022_06_28_172102_create_modelos_table.php
@@ -13,12 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('equipamentos', function (Blueprint $table) {
+        Schema::create('modelos', function (Blueprint $table) {
             $table->id();
-            $table->foreign('modelo_id')->references('id')->on('modelos');
-            $table->string(column:'denominacao');
-            $table->string(column:'placa');
-            $table->string(column:'chassi');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('equipamentos');
+        Schema::dropIfExists('modelos');
     }
 };
